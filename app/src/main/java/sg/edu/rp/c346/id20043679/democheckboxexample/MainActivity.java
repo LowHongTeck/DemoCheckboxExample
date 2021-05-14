767e5583efd5e22c0584f0117d68c6c9f1bb240f
LowHongTeck <20043679@myrp.edu.sg>
2021-05-14 13:03:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20043679/democheckboxexample/MainActivity.java
@@ -0,0 +1,40 @@
+package sg.edu.rp.c346.id20043679.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnable;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cbEnable = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_LONG).show();
+                if (cbEnable.isChecked()){
+                    tvShow.setText("The discount is given." );
+                } else{
+                    tvShow.setText("The discount is not given");
+                }
+            }
+        });
+    }
+}
